b5078736caf963fed0d2844e2bb8d20a0527a798
Mario-S <spindizzy@outlook.com>
2015-03-27 15:38:00 +0100

    exended controller

--- src/main/java/org/inno/control/TechNodeController.java
+++ src/main/java/org/inno/control/TechNodeController.java
@@ -1,7 +1,9 @@
 package org.inno.control;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import org.controlsfx.validation.ValidationSupport;
 import static org.controlsfx.validation.Validator.createEmptyValidator;
 
@@ -14,11 +16,15 @@ public class TechNodeController extends AbstractNodeController{
 
     @FXML
     private TextField txtLayer;
+    
+    @FXML
+    private ComboBox<Color> cmbState;
 
     @Override
     protected void initialize(ValidationSupport validationSupport, MessageFactory factory) {
         super.initialize(validationSupport, factory);
         validationSupport.registerValidator(txtLayer, createEmptyValidator(factory.getMessage("err.req.layer")));
+        validationSupport.registerValidator(cmbState, createEmptyValidator(factory.getMessage("err.req.state")));
     }
     
     
