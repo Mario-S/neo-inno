cd44123a9c2158ad62d01927942435c307e57987
Mario-S <spindizzy@outlook.com>
2015-04-06 16:07:12 +0200

    implemented nodes to tables

--- src/main/java/org/inno/control/RelationController.java
+++ src/main/java/org/inno/control/RelationController.java
@@ -1,14 +1,71 @@
 package org.inno.control;
 
+import java.util.Collection;
+import static javafx.collections.FXCollections.observableArrayList;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import org.inno.model.Project;
+import org.inno.model.Technology;
+import org.openide.util.Lookup.Result;
+import org.openide.util.LookupEvent;
+import org.openide.util.LookupListener;
+
 /**
- * Controller to create a relation between {@link Project} and {@link Technology}.
+ * Controller to create a relation between {@link Project} and
+ * {@link Technology}.
+ *
  * @author schroeder
  */
-public class RelationController extends AbstractController{
-    
+public class RelationController extends AbstractController {
+
+    private final ObservableList<Technology> techList;
+
+    private final ObservableList<Project> projList;
+
+    private final Result<Technology> techResult;
+
+    private final Result<Project> projResult;
+
+    @FXML
+    private TableView tableProj;
+
+    @FXML
+    private TableView tableTech;
+
+    public RelationController() {
+        techList = observableArrayList();
+        projList = observableArrayList();
+        techResult = getContext().getLookup().lookupResult(Technology.class);
+        projResult = getContext().getLookup().lookupResult(Project.class);
+        techResult.addLookupListener(new TechnologyResultListener());
+        projResult.addLookupListener(new ProjectResultListener());
+    }
 
     @Override
     void initialize(MessageFactory factory) {
+        tableTech.setItems(techList);
+        tableProj.setItems(projList);
     }
-    
+
+    private class TechnologyResultListener implements LookupListener {
+
+        @Override
+        public void resultChanged(LookupEvent le) {
+            Result<Technology> res = (Result<Technology>) le.getSource();
+            techList.clear();
+            techList.addAll(res.allInstances());
+        }
+    }
+
+    private class ProjectResultListener implements LookupListener {
+
+        @Override
+        public void resultChanged(LookupEvent le) {
+            Result<Project> res = (Result<Project>) le.getSource();
+            projList.clear();
+            projList.addAll(res.allInstances());
+        }
+
+    }
 }
