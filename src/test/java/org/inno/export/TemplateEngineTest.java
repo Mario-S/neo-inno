b4e635b94677b6c2bf4cd376fb3979e026a0d7d0
Mario-S <spindizzy@outlook.com>
2015-04-16 22:42:52 +0200

    template engine

---
+++ src/test/java/org/inno/export/TemplateEngineTest.java
@@ -0,0 +1,33 @@
+package org.inno.export;
+
+import java.util.Optional;
+import org.inno.model.Project;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author spindizzy
+ */
+public class TemplateEngineTest {
+    
+    private TemplateEngine classUnderTest;
+    
+    @Before
+    public void setUp() {
+        classUnderTest = new TemplateEngine();
+    }
+
+    /**
+     * Test of parse method, of class TemplateEngine.
+     */
+    @Test
+    public void testParse_Project() {
+        Project project = new Project();
+        project.setName("test");
+        Optional<String> result = classUnderTest.parse(project);
+        assertTrue(result.isPresent());
+    }
+    
+}
