5621c361f4791ab350a2edda234b643259129690
Mario-S <spindizzy@outlook.com>
2015-06-29 13:29:51 +0200

    added spring data annotations to model

--- src/main/java/org/inno/model/Node.java
+++ src/main/java/org/inno/model/Node.java
@@ -1,14 +1,24 @@
 package org.inno.model;
 
-import java.util.Objects;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
+import org.springframework.data.neo4j.annotation.GraphId;
+import org.springframework.data.neo4j.annotation.NodeEntity;
+
+import java.util.Objects;
+
+
 /**
- * Parent class which identifies a node
+ * Parent class which identifies a node.
+ *
  * @author spindizzy
  */
+@NodeEntity
 public class Node {
+    @GraphId
+    private Long id;
+
     private final StringProperty name = new SimpleStringProperty();
     private final StringProperty version = new SimpleStringProperty();
 
@@ -24,11 +34,11 @@ public class Node {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(final String name) {
         this.name.set(name);
     }
 
-    public void setVersion(String version) {
+    public void setVersion(final String version) {
         this.version.set(version);
     }
 
@@ -36,36 +46,56 @@ public class Node {
         return version;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(final Long id) {
+        this.id = id;
+    }
+
     @Override
     public int hashCode() {
         int hash = 7;
-        hash = 53 * hash + Objects.hashCode(this.getName());
-        hash = 53 * hash + Objects.hashCode(this.getVersion());
+        hash = (53 * hash) + Objects.hashCode(this.getId());
+        hash = (53 * hash) + Objects.hashCode(this.getName());
+        hash = (53 * hash) + Objects.hashCode(this.getVersion());
+
         return hash;
     }
 
     @Override
-    public boolean equals(Object obj) {
+    public boolean equals(final Object obj) {
         if (obj == null) {
             return false;
         }
+
         if (getClass() != obj.getClass()) {
             return false;
         }
+
         final Node other = (Node) obj;
+
         if (!hasEqualFields(other)) {
             return false;
         }
+
         return true;
     }
-    
-    boolean hasEqualFields(Node other){
+
+    boolean hasEqualFields(final Node other) {
+        if (!Objects.equals(this.getId(), other.getId())) {
+            return false;
+        }
+
         if (!Objects.equals(this.getName(), other.getName())) {
             return false;
         }
+
         if (!Objects.equals(this.getVersion(), other.getVersion())) {
             return false;
         }
+
         return true;
     }
 }
