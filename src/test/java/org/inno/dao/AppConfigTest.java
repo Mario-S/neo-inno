e12c989f0e5af7be02c2d2ac09b6cca6a71108fa
Mario-S <spindizzy@outlook.com>
2015-06-29 14:03:49 +0200

    added spring configuration

---
+++ src/test/java/org/inno/dao/AppConfigTest.java
@@ -0,0 +1,37 @@
+/*
+ * Created by Schuetze Consulting Informationssysteme AG, Berlin.
+ */
+package org.inno.dao;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+
+/**
+ * @author schroeder
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class, loader = AnnotationConfigContextLoader.class)
+public class AppConfigTest {
+    @Autowired
+    private GraphDatabaseService databaseService;
+
+    /**
+     * Test of graphDatabaseService method, of class AppConfig.
+     */
+    @Test
+    public void testGraphDatabaseService() {
+        assertNotNull(databaseService);
+    }
+}
